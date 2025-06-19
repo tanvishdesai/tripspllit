eaee8fd789016e9643828291cc1172346120220b
tanvishdesai <vikramdesai434@gmail.com>
2025-06-19 23:14:15 +0530

    next auth url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 
+                  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000')
+  }
 };
 
 export default nextConfig;
